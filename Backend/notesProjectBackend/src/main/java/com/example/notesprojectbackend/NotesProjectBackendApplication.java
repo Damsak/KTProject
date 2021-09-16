6b18d2de7dd141402c5f9c69864a8413005d5cf9
Damsak Bandara <damsakbandara@Damsaks-MacBook-Pro.local>
2021-09-16 14:56:13 +0530

    Added a demo react app and demo springboot app

---
+++ Backend/notesProjectBackend/src/main/java/com/example/notesprojectbackend/NotesProjectBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.notesprojectbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotesProjectBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NotesProjectBackendApplication.class, args);
+    }
+
+}
---
+++ Backend/notesProjectBackend/src/test/java/com/example/notesprojectbackend/NotesProjectBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.notesprojectbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotesProjectBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
